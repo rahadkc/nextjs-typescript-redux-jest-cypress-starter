e399a21755f36724dba96db6fa7af1aec34b3c4d
rahadkc <rahadkc@gmail.com>
2023-11-20 21:00:13 +0600

    Remove unnecessary code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,11 @@ import ReduxProvider from '../redux/provider'
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import SwrConfigProvider from '../http/swrConfigProvider'
-import { Toaster } from '../lib/toast'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ReduxProvider>
       <SwrConfigProvider>
-        <Toaster toastOptions={{ duration: 2000 }} />
         <Component {...pageProps} />
       </SwrConfigProvider>
     </ReduxProvider>
